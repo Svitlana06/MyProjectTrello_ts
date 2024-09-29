503de947b35666202a2f8c806d885180b36912d9
Svitlana Savchuk <svitlanasavchuk06@gmail.com>
2024-09-29 11:02:57 +0300

    fixed

--- src/features/step-definitions/create_card_steps.js
+++ src/features/step-definitions/create_card_steps.js
@@ -5,7 +5,7 @@ const { BoardPage } = require('../../po/pages/imports.js');
 
 const boardPage = new BoardPage();
 
-Given('I am on a Trello board with ', async () => {
+Given('I am on a Trello board with', async () => {
   await boardPage.boardComponent.check('existedBoard').click();
 });
 
@@ -17,4 +17,18 @@ When('I add a new card', async () => {
 
 Then('a new card should be added to the list', async () => {
   expect(await boardPage.boardComponent.check('card').isDisplayed()).to.be.true;
+});
+
+Given('I am on a Trello board', async () => {
+  await boardPage.boardComponent.check('existedBoard').click();
+});
+
+When('I add a new list', async () => {
+  await boardPage.boardComponent.createBtn('list').click();
+  await boardPage.boardComponent.input('list').setValue(valuesForFields.newListName);
+  await boardPage.boardComponent.addBtn('list').click();
+});
+
+Then('a new list should be added to the board', async () => {
+  expect(await boardPage.boardComponent.check('list').isDisplayed()).to.be.true;
 });
