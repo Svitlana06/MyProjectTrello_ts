097b566981e1b79da0189957866dd0cf69620de9
Svitlana Savchuk <svitlanasavchuk06@gmail.com>
2024-09-30 12:05:01 +0300

    fixed

--- src/features/support/hook.js
+++ src/features/support/hook.js
@@ -2,51 +2,51 @@ const { Before, After } = require('@cucumber/cucumber');
 const { inputData, url } = require('../../tests/settings');
 const { pages } = require('../../po');
 
+async function login() {
+  await pages('homePage').open();
+  await pages('homePage').headerHomeComponent.signInBtn.click();
+
+  await browser.waitUntil(
+    async () => (await browser.getUrl()).includes(url.login),
+    { timeout: 10000 }
+  );
+
+  await pages('registrationPage')
+    .registrationComponent.input('username')
+    .setValue(inputData.emailLogIn);
+  await pages('registrationPage').registrationComponent.submitBtn('signIn').click();
+
+  await browser.waitUntil(
+    async () => await pages('registrationPage').registrationComponent.input('password').isDisplayed(),
+    { timeout: 10000 }
+  );
+
+  await pages('registrationPage')
+    .registrationComponent.input('password')
+    .setValue(inputData.password);
+  await pages('registrationPage').registrationComponent.submitBtn('signIn').click();
+
+  await browser.waitUntil(
+    async () => (await browser.getUrl()).includes(url.boards),
+    { timeout: 15000 }
+  );
+}
+
 Before(async function (scenario) {
+  await browser.deleteCookies();
+  await browser.clearLocalStorage(); // Якщо використовуєте локальне сховище
   if (scenario.pickle.tags.some((tag) => tag.name === '@login')) {
     try {
-      await pages('homePage').open();
-      await pages('homePage').headerHomeComponent.signInBtn.click();
-
-      await browser.waitUntil(
-        async () => {
-          return (await browser.getUrl()).includes(url.login);
-        },
-        { timeout: 10000 }
-      );
-
-      await pages('registrationPage')
-        .registrationComponent.input('username')
-        .setValue(inputData.emailLogIn);
-      await pages('registrationPage').registrationComponent.submitBtn('signIn').click();
-
-      await browser.waitUntil(
-        async () => {
-          return await pages('registrationPage')
-            .registrationComponent.input('password')
-            .isDisplayed();
-        },
-        { timeout: 10000 }
-      );
-
-      await pages('registrationPage')
-        .registrationComponent.input('password')
-        .setValue(inputData.password);
-      await pages('registrationPage').registrationComponent.submitBtn('signIn').click();
-
-      await browser.waitUntil(
-        async () => {
-          return (await browser.getUrl()).includes(url.boards);
-        },
-        { timeout: 15000 }
-      );
+      await login();
     } catch (error) {
       console.error('Error in Before hook:', error);
     }
   }
 });
+
 After(async function () {
   await browser.deleteCookies();
+  await browser.reloadSession();
 });
 
 module.exports = { After, Before };
