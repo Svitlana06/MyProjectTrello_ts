9598ad4c09a61a6728be9ffb5718ae8788ca4e07
Svitlana Savchuk <svitlanasavchuk06@gmail.com>
2024-09-30 17:29:33 +0300

    added files

--- src/features/support/hook.js
+++ src/features/support/hook.js
@@ -1,30 +1,32 @@
 const { Before, After } = require('@cucumber/cucumber');
 const { inputData, url } = require('../../tests/settings');
-const { pages } = require('../../po');
+const { HomePage, RegistrationPage } = require('../../po/pages/imports.js');
 
 async function login() {
-  await pages('homePage').open();
-  await pages('homePage').headerHomeComponent.signInBtn.click();
+  const homePage = new HomePage();
+const registrationPage = new RegistrationPage();
+  await homePage.open();
+  await homePage.headerHomeComponent.signInBtn.click();
 
   await browser.waitUntil(
     async () => (await browser.getUrl()).includes(url.login),
     { timeout: 10000 }
   );
 
-  await pages('registrationPage')
+  await registrationPage
     .registrationComponent.input('username')
     .setValue(inputData.emailLogIn);
-  await pages('registrationPage').registrationComponent.submitBtn('signIn').click();
+  await registrationPage.registrationComponent.submitBtn('signIn').click();
 
   await browser.waitUntil(
-    async () => await pages('registrationPage').registrationComponent.input('password').isDisplayed(),
+    async () => await registrationPage.registrationComponent.input('password').isDisplayed(),
     { timeout: 10000 }
   );
 
-  await pages('registrationPage')
+  await registrationPage
     .registrationComponent.input('password')
     .setValue(inputData.password);
-  await pages('registrationPage').registrationComponent.submitBtn('signIn').click();
+  await registrationPage.registrationComponent.submitBtn('signIn').click();
 
   await browser.waitUntil(
     async () => (await browser.getUrl()).includes(url.boards),
