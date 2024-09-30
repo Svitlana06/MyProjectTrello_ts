fdcb5b7369617b3ae7738f8f1e2d646a202f0082
Svitlana Savchuk <svitlanasavchuk06@gmail.com>
2024-09-30 09:03:00 +0300

    fixed

--- src/config/wdio.cucumber.conf.js
+++ src/config/wdio.cucumber.conf.js
@@ -29,7 +29,7 @@ exports.config = {
   cucumberOpts: {
     require: [
       'src/features/support/hook.js',
-      // 'src/features/step-definitions/change_settings_steps.js',
+      'src/features/step-definitions/change_settings_steps.js',
       'src/features/step-definitions/create_board_steps.js',
       'src/features/step-definitions/create_card_steps.js',
       'src/features/step-definitions/create_list_steps.js',
