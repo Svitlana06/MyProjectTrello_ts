911f8698bef9304eac3b395e58302be912f4d584
Svitlana Savchuk <svitlanasavchuk06@gmail.com>
2024-09-30 11:59:39 +0300

    fixed

--- src/features/step-definitions/change_settings_steps.js
+++ src/features/step-definitions/change_settings_steps.js
@@ -15,17 +15,22 @@ When('I update workspace details', async () => {
   await accountPage.settingsComponent.openFrequencyProperty.waitForExist({
     timeout: 10000,
   });
-
+  await accountPage.settingsComponent.openFrequencyProperty.waitForDisplayed();
   await accountPage.settingsComponent.openFrequencyProperty.click();
 
   await accountPage.settingsComponent.changeFrequency.waitForExist({
     timeout: 10000,
   });
+  await accountPage.settingsComponent.changeFrequency.waitForDisplayed();
   await accountPage.settingsComponent.changeFrequency.click();
 });
 
 Then('the workspace settings should be updated successfully', async () => {
-  (await accountPage.settingsComponent.checkFrequency.getText()).should.equal(
-    valuesForFields.checkedFrequently
-  );
+  await accountPage.settingsComponent.checkFrequency.waitForExist({
+    timeout: 10000,
+  });
+  await accountPage.settingsComponent.checkFrequency.waitForDisplayed();
+  
+  const frequencyText = await accountPage.settingsComponent.checkFrequency.getText();
+  frequencyText.should.equal(valuesForFields.checkedFrequently);
 });
