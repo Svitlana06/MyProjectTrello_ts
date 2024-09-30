93c3780e31e95cf7d6a05b74e08917f40d0e2846
Svitlana Savchuk <svitlanasavchuk06@gmail.com>
2024-09-30 08:16:19 +0300

    fixed

--- src/features/step-definitions/create_card_steps.js
+++ src/features/step-definitions/create_card_steps.js
@@ -5,7 +5,7 @@ const { BoardPage } = require('../../po/pages/imports.js');
 
 const boardPage = new BoardPage();
 
-Given('I am on a Trello board with', async () => {
+Given('I am on a Trello board with existing lists', async () => {
   await boardPage.boardComponent.check('existedBoard').click();
 });
 
@@ -15,20 +15,6 @@ When('I add a new card', async () => {
   await boardPage.boardComponent.addBtn('card').click();
 });
 
-Then('a new card should be added', async () => {
+Then('a new card should be added to the list', async () => {
   expect(await boardPage.boardComponent.check('card').isDisplayed()).to.be.true;
-});
-
-Given('I dont know', async () => {
-  await boardPage.boardComponent.check('existedBoard').click();
-});
-
-When('O my god', async () => {
-  await boardPage.boardComponent.createBtn('list').click();
-  await boardPage.boardComponent.input('list').setValue(valuesForFields.newListName);
-  await boardPage.boardComponent.addBtn('list').click();
-});
-
-Then('a new list should be added', async () => {
-  expect(await boardPage.boardComponent.check('list').isDisplayed()).to.be.true;
 });
--- src/features/trello.feature
+++ src/features/trello.feature
@@ -1,9 +1,19 @@
 Feature: Trello Feature
 
-
+@login
+   Scenario: Creating a new card on a list
+   Given I am on a Trello board with existing lists
+   When I add a new card
+   Then a new card should be added to the list
 
 @login
    Scenario: Creating a new list
-   Given I dont know
-   When O my god
-   Then a new list should be added
+   Given I am on a Trello board
+   When I add a new list
+   Then a new list should be added to the board
+
+@login
+  Scenario: Modifying workspace settings
+  Given I am on the workspace settings page
+  When I update workspace details
+  Then the workspace settings should be updated successfully
